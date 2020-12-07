d6c3a0b908a54315be95a10ca0921fd346144219
Emre Sarikaya <emres@tuttur.com>
2020-12-07 19:38:08 +0300

    loginCases

--- src/test/java/com/tuttur/test/LoginTest.java
+++ src/test/java/com/tuttur/test/LoginTest.java
@@ -22,6 +22,7 @@ public class LoginTest extends BaseTest {
 
         new MainPage(driver).getLoginPage().login(prop.getObject("account_no")).checkAccountNo();
     }
+
     /**
      * Case 1.1
      * Kullanıcı adı ile başarılı login
@@ -31,6 +32,7 @@ public class LoginTest extends BaseTest {
     public void successfullyLoginWithUserName() throws IOException {
         new MainPage(driver).getLoginPage().login(prop.getObject("username")).checkUsernameText();
     }
+
     /**
      * Case 1.2
      * TCKN  ile başarılı login
@@ -39,16 +41,61 @@ public class LoginTest extends BaseTest {
     public void successfulllyLoginWithIdentityNo() throws IOException {
         new MainPage(driver).getLoginPage().login(prop.getObject("login_ssn")).checkUsernameText();
     }
+
     /**
      * Case 1.3
-     * Başarısız login kontrolü
+     * TCKN ile başarısız login kontrolü
      */
     @Test
-    public void failLogin() throws IOException {
-        new MainPage(driver).getLoginPage().login(prop.getObject("login_ssn"));
+    public void failLoginWithSsn() throws IOException {
+        new MainPage(driver).getLoginPage().login(prop.getObject("invalidSsn"));
         new LoginPage(driver).checkFailLogin();
-
     }
+
+    /**
+     * Case 1.4
+     * Username ile başarısız login kontrolü
+     */
+    @Test
+    public void failLoginWithUsername() throws IOException {
+        new MainPage(driver).getLoginPage().login(prop.getObject("invalidUsername"));
+        new LoginPage(driver).checkFailLogin();
+    }
+
+    /**
+     * Case 1.5
+     * Account no ile başarısız login kontrolü
+     */
+    @Test
+    public void failLoginWithAccountNo() throws IOException {
+        new MainPage(driver).getLoginPage().login(prop.getObject("invalidAccountNo"));
+        new LoginPage(driver).checkFailLogin();
+    }
+
+    /**
+     * Case 1.6
+     * Email ile başarısız login kontrolü
+     */
+    @Test
+    public void failLoginWithEmail() throws IOException {
+
+        new MainPage(driver).getLoginPage().login(prop.getObject("loginEmail"));
+        new LoginPage(driver).checkFailLogin();
+    }
+
+    /**
+     * Case 1.7
+     * Telefon numarası ile başarısız login kontrolü
+     */
+    @Test
+    public void failLoginWithPhoneNumber() throws IOException {
+
+        new MainPage(driver).getLoginPage().login(prop.getObject("invalidPhone"));
+        new LoginPage(driver).checkFailLogin();
+    }
+
+
+
 }
 
 
