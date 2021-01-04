bee5d823b17d2637e7d38bb9b188ae863b56a85d
gokhansangar <123456>
2021-01-04 17:51:57 +0300

    config.properties add header url

--- src/test/java/com/tuttur/constants/Navigation_Constants.java
+++ src/test/java/com/tuttur/constants/Navigation_Constants.java
@@ -1,10 +1,17 @@
 package com.tuttur.constants;
 
 import com.tuttur.util.BasePageUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import javax.swing.plaf.PanelUI;
 
 public class Navigation_Constants extends BasePageUtil {
     public Navigation_Constants(WebDriver driver) {
         super(driver);
     }
+
+    public By HEADER_IDDAA_BUTTON = By.cssSelector(".headerMenu-item");
+    public By IDDAA_SUBMENU = By.className("headerMenu-subMenu-item");
+
 }
--- src/test/java/com/tuttur/test/NavigationTest.java
+++ src/test/java/com/tuttur/test/NavigationTest.java
@@ -1,9 +1,19 @@
 package com.tuttur.test;
 
 import com.tuttur.base.BaseTest;
+import com.tuttur.page.NavigationPage;
+import org.junit.Test;
 
 public class NavigationTest extends BaseTest {
 
+
+
+    @Test
+    public void ıddaaSubmenuClick () {
+
+        new NavigationPage(driver).submenu();
+
+    }
 
 
 
