6ff56e89072aac3cf125853496ee25c3652ddb63
Emre Sarikaya <emres@tuttur.com>
2021-08-26 15:27:25 +0300

    BasePageUtil

--- src/test/java/com/tuttur/util/BasePageUtil.java
+++ src/test/java/com/tuttur/util/BasePageUtil.java
@@ -3,7 +3,6 @@ package com.tuttur.util;
 
 import org.junit.Assert;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
