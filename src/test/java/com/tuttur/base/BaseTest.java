f5a7b6e5d9c19bcfc9f65b9e2223d37c50fcdd32
Mehmet Ali Ergut <malie@tuttur.com>
2021-08-27 13:42:45 +0300

    Update BaseTest.java, revert --headless.

--- src/test/java/com/tuttur/base/BaseTest.java
+++ src/test/java/com/tuttur/base/BaseTest.java
@@ -59,7 +59,6 @@ public class BaseTest {
 				ChromeOptions options = new ChromeOptions();
 				System.setProperty("webdriver.chrome.driver", "properties/driver/chromedriverremote");
 				options.addArguments("--disable-dev-shm-usage");
-				options.addArguments("--headless");
 				options.merge(capabilities);
 				driver = new ChromeDriver(options);
 			}
