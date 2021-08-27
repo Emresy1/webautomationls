ca609905c725d330081ffc25fbe0bc03300842d6
Emre Sarikaya <emres@tuttur.com>
2021-08-27 10:29:54 +0300

    jenkins set up

--- src/test/java/com/tuttur/base/BaseTest.java
+++ src/test/java/com/tuttur/base/BaseTest.java
@@ -52,7 +52,7 @@ public class BaseTest {
 
 			if (browserName.equalsIgnoreCase("chrome")) {
 
-				System.setProperty("webdriver.chrome.driver", "properties/driver/chromedriverlinux");
+				System.setProperty("webdriver.chrome.driver", "properties/driver/chromedriver");
 				ChromeOptions options = new ChromeOptions();
 				options.merge(capabilities);
 				driver = new ChromeDriver(options);
